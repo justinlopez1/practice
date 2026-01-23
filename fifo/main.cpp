1724440fcb38556323dc8b727d556f13e8c0286c
Justin Lopez <justincolelopez1@gmail.com>
2026-01-22 19:12:48 -0500

    idk

--- fifo/main.cpp
+++ fifo/main.cpp
@@ -22,6 +22,7 @@
 #include <iostream>
 #include <string>
 #include <array>
+#include <semaphore>
 #include <stdexcept>
 
 template<typename T, size_t N>
@@ -29,19 +30,17 @@ class Fifo {
 public:
     Fifo() : head_(0), tail_(0), size_(0) {}
 
-    // isr safe, returns true if item was pushed
-    bool try_push(T item) {
-       if (size_ == N) {
-           return false;
-       }
+    bool push(T item) {
+        if (size_ == N) {
+            return false;
+        }
         arr_[tail_++] = item;
         if (tail_ == N) { tail_ = 0; }
         size_++;
         return true;
     }
 
-    // isr safe, returns true if item was popped
-    bool try_pop(T& out) {
+    bool pop(T& out) {
         if (size_ == 0) {
             return false;
         }
@@ -51,7 +50,6 @@ public:
         return true;
     }
 
-    // function variations that block (using mtx/cond) (non isr safe)
 
     size_t size() { return size_; }
     bool empty() { return size_ == 0; }
@@ -62,6 +60,9 @@ private:
     size_t head_; // front element
     size_t tail_; // insert at tail_ (one past last element)
     size_t size_;
+    std::mutex mtx_;
+    std::counting_semaphore<N> full_{0}; // num full spaces
+    std::counting_semaphore<N> empty_{N};
 };
 
 void testcase1();
@@ -74,15 +75,15 @@ int main() {
 void testcase1() {
     Fifo<int, 5> fifo;
     for (int i = 0; i < 5; i++) {
-        fifo.try_push(i);
+        fifo.push(i);
     }
     int out;
-    if (fifo.try_pop(out); out != 0) { std::cerr << "failed!" << std::endl; }
-    if (fifo.try_pop(out); out != 1) { std::cerr << "failed!" << std::endl; }
-    if (fifo.try_pop(out); out != 2) { std::cerr << "failed!" << std::endl; }
-    if (fifo.try_pop(out); out != 3) { std::cerr << "failed!" << std::endl; }
-    if (fifo.try_pop(out); out != 4) { std::cerr << "failed!" << std::endl; }
-    if (bool success = fifo.try_pop(out); success != false) { std::cerr << "failed!" << std::endl; }
+    if (fifo.pop(out); out != 0) { std::cerr << "failed!" << std::endl; }
+    if (fifo.pop(out); out != 1) { std::cerr << "failed!" << std::endl; }
+    if (fifo.pop(out); out != 2) { std::cerr << "failed!" << std::endl; }
+    if (fifo.pop(out); out != 3) { std::cerr << "failed!" << std::endl; }
+    if (fifo.pop(out); out != 4) { std::cerr << "failed!" << std::endl; }
+    if (bool success = fifo.pop(out); success != false) { std::cerr << "failed!" << std::endl; }
     if (!fifo.empty()) { std::cerr << "failed!" << std::endl; }
 }
 
